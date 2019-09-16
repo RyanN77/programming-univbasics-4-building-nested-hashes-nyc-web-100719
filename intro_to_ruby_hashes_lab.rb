389d8d17ee1a6b2c5cabf1881ab051a453901c72
adventurous-class-0665 <rngao97@gmail.com>
2019-09-16 22:05:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,11 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  {railroads: {pieces: 4, rent_in_dollars: {key1: "", key2: "", key3: "", key4: ""}, names: {railroad1: "", railroad2: "", railroad3: "", railroad4: ""}}}
+  {railroads: {pieces: 4, 
+  rent_in_dollars: {key1: "", key2: "", key3: "", key4: ""}, 
+  names: {railroad1: "", railroad2: "", railroad3: "", railroad4: ""}
+  one_piece_owned: {}
+  }}
 end
 
 def monopoly_with_fourth_tier
